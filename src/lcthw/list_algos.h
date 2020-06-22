a4bfaea1d3bfeff66254abfc1c5b505bd89f7845
Patrick-Cullinane <Patrick.Cullinane@ibm.com>
2020-06-22 11:02:04 -0400

    add merge_sort

---
+++ src/lcthw/list_algos.h
@@ -0,0 +1,18 @@
+#ifndef lcthw_List_algos_h
+#define lcthw_List_algos_h
+
+#include <lcthw/list.h>
+
+typedef int (*List_compare) (const void *a, const void *b);
+
+int List_bubble_sort(List * list, List_compare cmp);
+
+List *List_merge_sort(List * list, List_compare cmp);
+
+int test_list_merge(List * list);
+
+
+
+#endif
+
+
