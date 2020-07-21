cbb51cedd134dd2d6f642cc9ba61da697448d6d5
cullinap <cullinap@gmail.com>
2020-07-21 17:42:00 -0400

    add dynamic arrays

---
+++ src/lcthw/darray.c
@@ -0,0 +1,26 @@
+#include <lcthw/darray.h>
+#include <assert.h>
+
+
+DArray *DArray_create(size_t element_size, size_t initial_max) {
+
+	DArray *array = malloc(sizeof(DArray));
+	check_mem(array);
+	array->max = initial_max;
+	check(array->max > 0, "You must set an initial_max > 0");
+
+	array->contents = calloc(initial_max, sizeof(void *));
+	check_mem(array->contents);
+
+	array->end = 0;
+	array->element_size = element_size;
+	array->expand_rate = DEFAULT_EXPAND_RATE;
+
+	return array;
+
+error:
+	
+	if (array)
+		free(array);
+	return NULL;
+}
